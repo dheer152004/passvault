61ecb8b701cc9df952d0f3239c5c28a876a1777b
dheer152004 <dheerajkumar152004@outlook.com>
2026-01-16 11:43:14 +0530

    Faet: Index Added

---
+++ src/pages/Index.tsx
@@ -0,0 +1,20 @@
+import { Layout } from "@/components/layout/Layout";
+import { HeroSection } from "@/components/home/HeroSection";
+import { FeaturesSection } from "@/components/home/FeaturesSection";
+import { HowItWorksSection } from "@/components/home/HowItWorksSection";
+import { TestimonialsSection } from "@/components/home/TestimonialsSection";
+import { CTASection } from "@/components/home/CTASection";
+
+const Index = () => {
+  return (
+    <Layout>
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <TestimonialsSection />
+      <CTASection />
+    </Layout>
+  );
+};
+
+export default Index;
